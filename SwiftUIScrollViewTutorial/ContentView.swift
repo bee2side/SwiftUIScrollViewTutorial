3ca6c1ee8324ad44b49c04955d606ead1b61c887
bee2side <bee2side@gmail.com>
2020-07-09 22:56:35 +0900

    Initial Commit

---
+++ SwiftUIScrollViewTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIScrollViewTutorial
+//
+//  Created by Doris on 2020/07/09.
+//  Copyright © 2020 kakao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
